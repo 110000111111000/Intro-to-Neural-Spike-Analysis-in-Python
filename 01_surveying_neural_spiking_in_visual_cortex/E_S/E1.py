b5981c93fef35350018bdc1249c709ac6905062e
110000111111000 <vajiheh.sabzali@gmail.com>
2025-07-08 12:02:54 +0200

    hello:)

--- 01_surveying_neural_spiking_in_visual_cortex/E_S/E1.py
+++ 01_surveying_neural_spiking_in_visual_cortex/E_S/E1.py
@@ -4,3 +4,12 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 import pingouin as pg
      
+import requests
+
+url = "https://uni-bonn.sciebo.de/s/mLMkb2TwbNx3Yg6"
+fname = "flash_spikes.parquet"
+response = requests.get(f"{url}/download")
+print("Downloading Data ...")
+with open(fname, "wb") as file:
+    file.write(response.content)
+print("Done!")     

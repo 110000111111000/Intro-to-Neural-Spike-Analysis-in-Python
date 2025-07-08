c016f1d6278387afbfe547253cd46e46f95c4714
110000111111000 <vajiheh.sabzali@gmail.com>
2025-07-08 12:50:48 +0200

    E5 done

--- 01_surveying_neural_spiking_in_visual_cortex/E_S/E5.py
+++ 01_surveying_neural_spiking_in_visual_cortex/E_S/E5.py
@@ -6,13 +6,13 @@ import pingouin as pg
      
 import requests
 
-url = "https://uni-bonn.sciebo.de/s/mLMkb2TwbNx3Yg6"
-fname = "flash_spikes.parquet"
-response = requests.get(f"{url}/download")
-print("Downloading Data ...")
-with open(fname, "wb") as file:
-    file.write(response.content)
-print("Done!")    
+#url = "https://uni-bonn.sciebo.de/s/mLMkb2TwbNx3Yg6"
+#fname = "flash_spikes.parquet"
+#response = requests.get(f"{url}/download")
+#print("Downloading Data ...")
+#with open(fname, "wb") as file:
+#    file.write(response.content)
+#print("Done!")    
 
 ## Exercise 1
 df = pd.read_parquet("flash_spikes.parquet")
@@ -34,7 +34,11 @@ print ("name of columns", df.columns)
 
 ## Exercise 4
 brain_area = df["brain_area"]
-print("brain_area",brain_area)
+#print("brain_area",brain_area)
 all_brain_area_recorded = df.brain_area.unique()
 print ("all_brain_area_recorded", all_brain_area_recorded)
- 
+
+##Exercise 5
+unit =df["unit_id"]
+Total_number_of_units_recorded =  len(unit)
+print("Total_number_of_units_recorded:",Total_number_of_units_recorded )

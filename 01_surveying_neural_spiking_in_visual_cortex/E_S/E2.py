1106ef212e88f20ffac29d7c1d2e89d7c894560c
110000111111000 <vajiheh.sabzali@gmail.com>
2025-07-08 12:17:50 +0200

    E2 done

---
+++ 01_surveying_neural_spiking_in_visual_cortex/E_S/E2.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+from matplotlib import pyplot as plt
+import seaborn as sns
+import pingouin as pg
+     
+import requests
+
+url = "https://uni-bonn.sciebo.de/s/mLMkb2TwbNx3Yg6"
+fname = "flash_spikes.parquet"
+response = requests.get(f"{url}/download")
+print("Downloading Data ...")
+with open(fname, "wb") as file:
+    file.write(response.content)
+print("Done!")    
+
+## Exercise 1
+df = pd.read_parquet("flash_spikes.parquet")
+#print (df.head(5))
+#print("All possible questions : 1- what is brain area?LM ")
+#print("All possible questions : 2- what is unit for spike_time? Second")
+
+## Exercise 2 
+print ("number of row or spikes in data",df.shape) 
+

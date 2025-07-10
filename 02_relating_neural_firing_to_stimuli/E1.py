62da96c5b6b70138ce2c38dd659a614b8c8b120e
110000111111000 <vajiheh.sabzali@gmail.com>
2025-07-10 14:25:19 +0200

    E1 done

--- 02_relating_neural_firing_to_stimuli/E1.py
+++ 02_relating_neural_firing_to_stimuli/E1.py
@@ -7,19 +7,28 @@ from scipy.stats import wilcoxon
 ## https://www.youtube.com/watch?v=2AqoK8itEFQ related to wilcoxon test
 
 ##1.Preparation
-import requests
+#import requests
 
-urls = [
-    "https://uni-bonn.sciebo.de/s/G64EkHoQkeZeoLm",
-    "https://uni-bonn.sciebo.de/s/mLMkb2TwbNx3Yg6",
-]
+#urls = [
+#    "https://uni-bonn.sciebo.de/s/G64EkHoQkeZeoLm",
+#    "https://uni-bonn.sciebo.de/s/mLMkb2TwbNx3Yg6",
+#]
 
-fnames = ["flash_stimuli.parquet", "flash_spikes.parquet"]
+#fnames = ["flash_stimuli.parquet", "flash_spikes.parquet"]
 
-for url, fname in zip(urls, fnames):
-    response = requests.get(f"{url}/download")
-    print("Downloading Data ...")
-    with open(fname, "wb") as file:
-        file.write(response.content)
-    print("Done!")
+#for url, fname in zip(urls, fnames):
+#    response = requests.get(f"{url}/download")
+#    print("Downloading Data ...")
+#    with open(fname, "wb") as file:
+#        file.write(response.content)
+#    print("Done!")
+
+#### E1
+spikes = pd.read_parquet("flash_spikes.parquet")
+#print(df.head(5))
+print(spikes.head(5)) 
+stimuli = pd.read_parquet("flash_stimuli.parquet")
+print(stimuli.head(10))
+
+
     
